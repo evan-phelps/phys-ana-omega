44303982d30ea1376ce4d06b3907fefd0d2fd696
Evan Phelps <evan.phelps@gmail.com>
2013-05-20 17:36:59 -0400

    test if PYTHONPATH already includes project's python directory

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 export WSOMEGA=$(dirname `which "${BASH_SOURCE[0]}"`)
-export PYTHONPATH=$PYTHONPATH:$WSOMEGA/python
+WSPYTHON=$WSOMEGA/python
+test `echo $PYTHONPATH | grep -c $WSPYTHON` -gt 0 || export PYTHONPATH=$PYTHONPATH:$WSPYTHON
