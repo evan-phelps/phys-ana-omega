0a4edfb3e5a222d050cfc6911b3387c2096ba941
Evan Phelps <evan.phelps@gmail.com>
2013-05-19 23:39:58 -0400

    added epfuncs module docstring changed epfuncs.f_gausexp limit arguments to ordered pairs (tuples)

--- python/epfuncs.py
+++ python/epfuncs.py
@@ -1,14 +1,24 @@
+"""Contains functions to be used for modeling and fitting, specifically with
+ROOT.  Function naming conventions:
+
+    + "f_*": return TF1 objects for fitting.
+
+    + "d_*": function used by TF1.  arguments bound to Double_t*, Double_t*
+    for coordinate and parameter vectors.
+
+"""
+
 from ROOT import TMath as m
 from ROOT import TF1
 
 
 def d_gausexp(v, par):
-    """convolution of gauss and exponential using analytic expression.
+    """Convolution of gauss and exponential using analytic expression.
     parameters:
         par[0] (mag):    magnitude
         par[1] (gmean):  gauss mean
         par[2] (gsigma): gauss sqrt(variance)
-        par[3] (ehl):  exponential decay half-life
+        par[3] (ehl):  exponential decay half-life.
     """
     x, mag, gmean, gsigma, ehl = v[0], par[0], par[1], par[2], par[3]
     assert mag > 0
@@ -20,18 +30,18 @@ def d_gausexp(v, par):
     return mag * factor * m.Erfc(erfcarg)
 
 
-def f_gausexp(fn='fgexp', rLo=0.4, rHi=2, magLo=0, magHi=100000,
-              meanLo=0.773, meanHi=0.793, sigmaLo=0.010, sigmaHi=0.030,
-              ehlLo=0.01, ehlHi=0.2):
+def f_gausexp(fn='fgexp', funcrange=(0.4, 2), limsmag=(0, 100000),
+              limsmean=(0.773, 0.793), limssigma=(0.010, 0.030),
+              limsehl=(0.01, 0.2)):
     """Wraps creation of TF1 corresponding to d_gausexp.  Default values and
     parameter limits are meant to be suitable for e1f omega mass range, but
-    note that the magHi parameter can change dramatically depending on binning
-    choices.
+    note that the limsmag[1] parameter can change dramatically depending on
+    binning choices.
     """
-    f = TF1('fgexp', d_gausexp, rLo, rHi, 4)
-    f.SetParLimits(0, magLo, magHi)
-    f.SetParLimits(1, meanLo, meanHi)
-    f.SetParLimits(2, sigmaLo, sigmaHi)
-    f.SetParLimits(3, ehlLo, ehlHi)
-    f.SetParameters(magHi, meanLo, sigmaLo, ehlLo)
+    f = TF1('fgexp', d_gausexp, funcrange[0], funcrange[1], 4)
+    f.SetParLimits(0, limsmag[0], limsmag[1])
+    f.SetParLimits(1, limsmean[0], limsmean[1])
+    f.SetParLimits(2, limssigma[0], limssigma[1])
+    f.SetParLimits(3, limsehl[0], limsehl[1])
+    f.SetParameters(limsmag[1], limsmean[0], limssigma[0], limsehl[0])
     return f
