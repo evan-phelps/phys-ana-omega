5346eb02969b414ece2290ff73e49e9fe764d351
Evan Phelps <evan.phelps@gmail.com>
2013-09-24 18:45:01 -0400

    reduced 3pi threshold a little bit

--- e1f6/DH_RunQuality.cpp
+++ e1f6/DH_RunQuality.cpp
@@ -210,7 +210,7 @@ void DH_RunQuality::FillHists(H10 *d)
 				lvPim->SetXYZM(d->p[ipart]*d->cx[ipart], d->p[ipart]*d->cy[ipart], d->p[ipart]*d->cz[ipart], MASS_PIM);
 				TLorentzVector lvMMPPIP = lvMMP - *lvPip;
                 TLorentzVector lvMMPPIM = lvMMP - *lvPim;
-                if (lvMMPPIP.M()>0.139*2 && lvMMPPIM.M()>0.139*2) {
+                if (lvMMPPIP.M()>0.135*2 && lvMMPPIM.M()>0.135*2) {
                 	TLorentzVector lvMMP = (*lvE0 - *lvE1 + *lvP0 - *lvP1);
                     TLorentzVector lvMMPPIPPIM = lvMMP - *lvPip - *lvPim;
                     hmmppippim_V_mmp->Fill(lvMMP.M(), lvMMPPIPPIM.M());
