642c472218b36e573a2ea0acfe8b740830bbb37d
Evan Phelps <evan.phelps@gmail.com>
2014-01-02 09:40:33 -0500

    main

--- e1f6/processH10s.cpp
+++ e1f6/processH10s.cpp
@@ -13,26 +13,37 @@
 #include "DH_Eid.h"
 #include "DH_SC_Hists_PrePid.h"
 #include "DH_CC_Hists.h"
+#include "DH_W_Skim.h"
+#include "DH_MMp_Skim.h"
 
 int main(int argc, char const *argv[])
 {
-    TFile *fout = new TFile("skim.root","recreate");
-    TChain *c = new TChain("h10");
+    TChain *c = new TChain("h10clone/h10");
     c->Add("*.root");
-    H10 *h10proc = new H10(c, "e1f");
+    TFile *fout = new TFile("skim.root","recreate");
+    H10 *h10proc = new H10(c, "input.e1f.exp.parms");
     h10proc->Add(new DH_Hists_Monitor("mon_raw", fout));
-    h10proc->Add(new DH_EC_Hists_PreEid("echists_raw", fout));
-    h10proc->Add(new DH_CC_Hists("cchists_raw", fout));
-    h10proc->Add(new DH_RunQuality("runquality", fout));
-    h10proc->Add(new DH_CC_Hists("cchists_qskim", fout));
     h10proc->Add(new DH_EC_Hists("echists_qskim", fout));
+    h10proc->Add(new DH_RunQuality("runquality", fout));
+    h10proc->Add(new DH_EC_Hists_PreEid("echists_raw", fout));
+    h10proc->Add(new DH_EC_Hists("echists_qskim_2", fout));
     h10proc->Add(new DH_Hists_Monitor("mon_qskim", fout));
     h10proc->Add(new DH_Eid("eid", fout));
     h10proc->Add(new DH_EC_Hists("echists_eskim", fout));
-    h10proc->Add(new DH_CC_Hists("cchists_eskim", fout));
     h10proc->Add(new DH_Hists_Monitor("mon_eskim", fout));
-    h10proc->Add(new DH_CloneH10("h10_eskim", fout));;
     h10proc->Add(new DH_SC_Hists_PrePid("scpid", fout));
+    h10proc->Add(new DH_W_Skim("w_skim", fout));
+    h10proc->Add(new DH_MMp_Skim("mmp_skim", fout));
+    h10proc->Add(new DH_CloneH10("h10clone", fout));
+    printf("all handlers added; execute h10proc.Loop\n");
     h10proc->Loop(-1,kFALSE);
     return 0;
 }
+
+/*
+g++ `root-config --cflags --glibs` -c H10.C
+g++ `root-config --cflags --glibs` -c DH_RunQuality.cpp
+g++ `root-config --cflags --glibs` -c Config.cpp
+g++ `root-config --cflags --glibs` -c HandlerChain.cpp
+g++ `root-config --cflags --glibs` *.o processH10s.cpp -o processH10s
+*/
