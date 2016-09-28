356214b6b16de96733fe1b2c8254618a86c4ad39
Evan Phelps <evan.phelps@gmail.com>
2016-09-27 22:51:24 -0400

    efid worksheet in progress

--- worksheets/util_draw.py
+++ worksheets/util_draw.py
@@ -23,13 +23,14 @@ def draw(hist, ncols=1, nrows=1, cell=1, fig=None,
         rplt.hist2d(hist, axes=subplot, **kwargs)
     else:
         rplt.errorbar([hist], xerr=False, emptybins=False, **kwargs)
-    # plt.show()
+    fig.set_tight_layout(True)
     return fig
 
-def mdraw(hists, ncols=1, nrows=1, **kwargs):
-    fig = plt.figure(figsize=goldenaspect(6.5, nrows, ncols))
+def mdraw(hists, ncols=1, nrows=1, figwidth=6.5, **kwargs):
+    fig = plt.figure(figsize=goldenaspect(figwidth, nrows, ncols))
     for (ih, h) in enumerate(hists):
-        draw(h, ncols, nrows, ih, fig=fig)
+        draw(h, ncols, nrows, ih+1, fig=fig)
+    fig.set_tight_layout(True)
     return fig
 
 def sdraw(hists, xlabel='', ylabel='', xlims=None, ylims=None, space=0):
@@ -57,5 +58,61 @@ def sdraw(hists, xlabel='', ylabel='', xlims=None, ylims=None, space=0):
     plt.xlabel(xlabel)
     return fig
 
-# u'$\Delta t$ (ns) assuming proton mass'
-# u'momentum (GeV)'
+
+def norm_x_slices(h2):
+    h2c = h2.Clone()
+    nby = h2c.GetNbinsY()
+    for xbin in range(1, h2c.GetNbinsX()+1):
+        h1 = h2c.ProjectionY('%s_%d'%(h2c.GetName(),xbin), xbin, xbin)
+        maxval = h1.GetMaximum()
+        for ybin in range(1, nby+1):
+            bc = h2c.GetBinContent(xbin,ybin)
+            if bc > 0:
+                h2c.SetBinContent(h2c.GetBin(xbin,ybin), bc/maxval)
+    return asrootpy(h2c)
+
+
+def norm_y_slices(h2):
+    h2c = h2.Clone()
+    nbx = h2c.GetNbinsX()
+    for ybin in range(1, h2c.GetNbinsY()+1):
+        h1 = h2c.ProjectionX('%s_%d'%(h2c.GetName(),ybin), ybin, ybin)
+        maxval = h1.GetMaximum()
+        for xbin in range(1, nbx+1):
+            bc = h2c.GetBinContent(xbin,ybin)
+            if bc > 0:
+                h2c.SetBinContent(h2c.GetBin(xbin,ybin), bc/maxval)
+    return asrootpy(h2c)
+
+
+def get_x_slices(h2, window=1):
+    hs = []
+    for xbin in range(1, h2.xaxis.get_nbins()+1):
+        h = h2.projection_y('%s_%d'%(h2.name, xbin), xbin, xbin+window-1)
+        if sum(h) < 10:
+            h = None
+        hs.append((h2.xaxis.get_bin_center(xbin),h))
+    return hs
+
+
+def get_peak_points(h2, xlo, xhi, ylo=0):
+    points = []
+    hs = get_xslices(h2)
+    pfinder = R.TSpectrum(1)
+    for h in hs:
+        if h[1] is not None and h[0]>xlo and h[0]<xhi:
+            pfinder.Search(h[1], 2.5, 'goff')
+            y = pfinder.GetPositionX()[0]
+            if y > ylo:
+                points.append((h[0],pfinder.GetPositionX()[0]))
+    return points
+
+
+def get_pointgen(gr, xlo, xhi):
+    def genpoints():
+        for x, y in zip(gr.x(), gsfmeans.y()):
+            if x > xlo and x < xhi and y > 0:
+                yield (x, y)
+    return genpoints
+
+
